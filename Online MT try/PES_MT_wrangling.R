c1be269b7ae7090dfc61ed280397f3207cd9f9cc
Paul Stillman <paulstillman@users.noreply.github.com>
2021-03-26 09:50:15 -0400

    Trial information calculated

--- Online MT try/PES_MT_wrangling.R
+++ Online MT try/PES_MT_wrangling.R
@@ -13,24 +13,25 @@ data_raw$xPos[[1]]
 
 data_clean <- data_raw %>% 
   mutate(x_pos_list_str = str_split(xPos, pattern = 'a'),
-         x_pos_list_numeric = map(x_pos_list_str, as.numeric),
+         x_pos = map(x_pos_list_str, as.numeric),
          y_pos_list_str = str_split(yPos, pattern = 'a'),
-         y_pos_list_numeric = map(y_pos_list_str, as.numeric),
-         length_x = map_dbl(x_pos_list_numeric, length),
-         length_y = map_dbl(y_pos_list_numeric, length),
+         y_pos = map(y_pos_list_str, as.numeric),
          time_list_str = str_split(time, pattern = 'a'),
-         length_t = map_dbl(time_list_str, length),
-         submit_t
-          )
+         time_list = map(time_list_str, as.numeric),
+         length_x = map_dbl(x_pos, length),
+         length_y = map_dbl(y_pos, length),
+         length_t = map_dbl(time_list, length),
+         load_t = str_split(onLoadTime, pattern = 'a') %>% map(as.numeric),
+         ready_t = str_split(onReadyTime, pattern = 'a') %>% map(as.numeric),
+         click_t = str_split(buttonClickTime, pattern = 'a') %>% map(as.numeric),
+         latency_list = str_split(latency, pattern = 'a') %>% map(as.numeric),
+         subject = row_number())
 
-data_clean$length_x == data_clean$length_y
-# okay so x-y pairs always have the same length, that's good
+data_long <- data_clean %>% 
+  select(subject, x_pos, y_pos, time_list, load_t, ready_t, click_t) %>% 
+  unnest(c(x_pos, y_pos, time_list)) %>% 
+  mutate(ready_time_lower = map2(ready_t, time_list, ~ .x < .y),
+         click_time_higher = map2(click_t, time_list, ~ .x > .y),
+         trial = map2_dbl(ready_time_lower, click_time_higher, ~ which(.x & .y)))
 
 
-data_clean$x_pos_list_str[[1]]
-
-data_clean %>% 
-  filter(index == 1) %>% 
-  unnest(c(x_pos_list_numeric, y_pos_list_numeric)) %>% 
-  ggplot(aes(x = x_pos_list_numeric, y = -y_pos_list_numeric)) +
-  geom_point() 
